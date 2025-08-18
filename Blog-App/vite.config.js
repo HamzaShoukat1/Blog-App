2e08bec20a0dc538ccf493ded17743c4f0ad3902
hamza <hamza123ali45678@gmail.com>
2025-08-19 00:09:59 +0500

    a

--- Blog-App/vite.config.js
+++ Blog-App/vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+// vite.config.mjs
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'] // Add any problematic deps
+    }
+  }
 });
-
-
-
-
-
