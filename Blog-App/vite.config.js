a27d8071a8337e5c85fc935fcc5bf6e9a791ad4b
hamza <hamza123ali45678@gmail.com>
2025-08-18 22:00:03 +0500

    chnages

--- Blog-App/vite.config.js
+++ Blog-App/vite.config.js
@@ -3,28 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-module.exports = defineConfig({
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
   plugins: [react(),tailwindcss()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'lib/main.js'),
-      name: 'MyLib',
-      fileName: (format) => `my-lib.${format}.js`
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
-})
+});
+
 
 
 
