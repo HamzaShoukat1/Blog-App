aecbabb556c0cb9adffb8f62dac8efb563fddde4
hamza <hamza123ali45678@gmail.com>
2025-08-20 15:55:52 +0500

    add

--- Blog-App/vite.config.js
+++ Blog-App/vite.config.js
@@ -3,10 +3,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'] // Add any problematic deps
-    }
-  }
+  plugins: [react()]
 });
