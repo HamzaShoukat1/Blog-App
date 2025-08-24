fa05ef0f6c4d291df23ad9e42235fc8aa16b32b2
hamza <hamza123ali45678@gmail.com>
2025-08-24 16:04:34 +0500

    change loader

--- Blog-App/src/Loader/Loaderworking.jsx
+++ Blog-App/src/Loader/Loaderworking.jsx
@@ -1,30 +1,50 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import useLoader from "../Loader/Loader";
+import { AnimatePresence, motion } from "framer-motion";
+import '../../src/index.css'
 export const Loaderworking =  ()=>{
   
   const location = useLocation()
 
   const {loading, setLoading} = useLoader()
+  
 
   useEffect(() => {
     setLoading(true)
     const timer = setTimeout(()=>{
       setLoading(false)
 
-    },300)
+    },400)
     return ()=>{
       clearTimeout(timer)
     }
    
   }, [location])
-  useEffect(() => {
-   if(loading){
-    document.body.style.overflow = 'hidden'
-   } else{
-    document.body.style.overflow = ''
-   }
-  }, [loading])
+ 
+   useEffect(() => {
+    document.body.style.overflow = loading ? "hidden" : "";
+  }, [loading]);
+  return (
+  
+    <AnimatePresence>
+      {loading && (
+          <motion.div
+          key='loader'
+      className="loader-wrapper"
+      initial={{opacity:0}}
+      animate={{opacity: 1}}
+      exit={{opacity:0}}
+      transition={{duration:0.4, ease: 'easeOut'}}
+      >
+     <div className="loader"></div>
+      </motion.div>
+      )}
+    
+    </AnimatePresence>
+  )
+  
+};
   
   
 
@@ -32,6 +52,5 @@ export const Loaderworking =  ()=>{
 
 
 
-}
-// export default Loaderworking
+
 export default Loaderworking
--- Blog-App/src/Loader/Loaderwrapper.jsx
+++ Blog-App/src/Loader/Loaderwrapper.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { LoaderProvider } from "./Loader";
 import  {Loader}  from "../Loader/Loader.jsx";
-import React from "react";
 export const  Loaderwrapper = ({children})=>{
   const [loading, setLoading] = useState(false)
 
